465e2074203d8512d55ece3eaead9d3b44c0fbb0
Federico Montes <federicomdo97@gmail.com>
2023-07-08 03:09:45 -0300

    using changelog style template

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,22 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
-  ],
+  content: ['./{src,mdx}/**/*.{js,mjs,jsx,mdx}'],
+  darkMode: 'class',
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontSize: {
+        '2xs': '.6875rem',
+      },
+      fontFamily: {
+        sans: ['Inter', ...defaultTheme.fontFamily.sans],
+        display: ['Mona Sans', ...defaultTheme.fontFamily.sans],
+      },
+      opacity: {
+        2.5: '0.025',
+        7.5: '0.075',
+        15: '0.15',
       },
     },
   },
